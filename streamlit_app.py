a882533148f7e831e41b258019092d0ce220ad6b
sebastiandres <sebastiandres@gmail.com>
2022-02-24 20:14:49 -0300

    Better alignment

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,7 +16,7 @@ def main():
     c1, c2 = st.columns([5,5])
     with c1:
         st.code("""
-            stb.true_or_false("Are you a robot?", False)
+stb.true_or_false("Are you a robot?", False)
             """)
     with c2:
         stb.true_or_false("Are you a robot?", False)
@@ -26,10 +26,10 @@ def main():
     c1, c2 = st.columns([5,4])
     with c1:
         st.code("""
-            stb.true_or_false("Are you a cyborg?", False, 
-                                success='Pfiuuuuu!!!', 
-                                error='RoBoTs NoT WeLcOmE to tHiS aPp', 
-                                button='Check MY answer')
+stb.true_or_false("Are you a cyborg?", False, 
+                  success='Pfiuuuuu!!!', 
+                  error='RoBoTs NoT WeLcOmE to tHiS aPp', 
+                  button='Check MY answer')
             """)
     with c2:
         stb.true_or_false("Are you a cyborg?", False, 
@@ -42,27 +42,27 @@ def main():
     c1, c2 = st.columns([4,3])
     with c1:
         st.code("""
-        checked_answer, correct_answer = stb.true_or_false("Are you a cyborg robot?", 
-                                                    False, 
-                                                    success="",
-                                                    error="",
-                                                    button='Check THIS answer')
-        if checked_answer:
-            if correct_answer:
-                st.info("Welcome fellow human!")            
-                st.balloons()
-            else:
-                N = 10.0
-                pb = st.progress(0.0)
-                ph = st.empty()
-                robot_message = ""
-                for i in range(1, int(N+1)):
-                    pb.progress(i/N)
-                    robot_message += random.choice(["Bip ", "bip ", "Bop ", "bop "])
-                    ph.code(robot_message)
-                    time.sleep(.5)
-        else:
-            st.write("You need to check the answer")
+checked_answer, correct_answer = stb.true_or_false("Are you a cyborg robot?", 
+                                                   False, 
+                                                   success="",
+                                                   error="",
+                                                   button='Check THIS answer')
+if checked_answer:
+    if correct_answer:
+        st.info("Welcome fellow human!")            
+        st.balloons()
+    else:
+        N = 10.0
+        pb = st.progress(0.0)
+        ph = st.empty()
+        robot_message = ""
+        for i in range(1, int(N+1)):
+            pb.progress(i/N)
+            robot_message += random.choice(["Bip ", "bip ", "Bop ", "bop "])
+            ph.code(robot_message)
+            time.sleep(.5)
+else:
+    st.write("You need to check the answer")
             """)
     with c2:
         checked_answer, correct_answer = stb.true_or_false("Are you a cyborg robot?", 
